561fafa51bd00d2e706bb3b69dd755f0a4656b83
Raxit Jain <rjain@ztech.io>
2018-01-08 14:45:08 +0530

    Added two assignments

---
+++ Java/PlacementApplication/src/org/zilker/raxit/PlacementDAO.java
@@ -0,0 +1,18 @@
+package org.zilker.raxit;
+
+import java.sql.SQLException;
+
+public interface PlacementDAO {
+	public void insertStudentDetails(StudentDetails studentdet) throws SQLException;
+	public void insertCompanyDetails(CompanyDetails companydet) throws SQLException;
+	public void updateStudentDetails(StudentDetails studentDetails) throws SQLException;
+	public void updateCompanyDetails(CompanyDetails companyDetails) throws SQLException;
+	public void enterStudentsPlaced(int companyid,int regno) throws SQLException;
+	public boolean checkEligibilty(int regno, int companyid) throws SQLException;
+	public void displayCompanyDetails(int companyid) throws SQLException;
+	public void displayStudentDetails(int regno) throws SQLException;
+	public void displayDetailsDepartment(int flag, String deptname) throws SQLException;
+	public boolean validateTeacher(String department, String password) throws SQLException;
+	public void noOfStudentsPlaced(String deptName) throws SQLException;
+	public void placementPercentage(String deptName) throws SQLException;
+}
